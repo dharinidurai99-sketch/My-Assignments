e2252c41822d0bde3aacf9311332453e1df3267f
Bhavadharini Duraisamy <dharinidurai99@gmail.com>
2026-03-01 09:15:44 +0530

    Add files via upload
    Day 01

---
+++ learnprimitivedata.js
@@ -0,0 +1,19 @@
+var firstname = "Dhari"
+var companyname = "Microsoft"
+var mobilenumber = 123456700
+var isautomation = true
+var hasplaywright 
+
+console.log("firstname:"+firstname)
+console.log("companyname:"+companyname)
+console.log("mobilenumber:"+mobilenumber)
+console.log("isautomation:"+isautomation)
+console.log("has a playwright:"+typeof(hasplaywright))
+
+console.log(lastname) //undefined
+var lastname ='A';
+
+function greet(){
+    var age = 29
+    console.log("age:"+age)
+}
